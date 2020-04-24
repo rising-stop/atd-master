d2f3f037a0731b18c4630cf8db22ca1af491e078
rising-stop <dango_liang@qq.com>
2020-04-24 09:55:16 +0800

    增加锁相关RAII类

--- modules/common/utility/ipc/semaphore.h
+++ modules/common/utility/ipc/semaphore.h
@@ -1,5 +1,7 @@
 #include <sys/sem.h>
 
+#include <type_traits>
+
 #ifndef __SEMAPHORE_H__
 #define __SEMAPHORE_H__
 
@@ -66,17 +68,38 @@ class Shared_SemMutex : public SemMutex {
   ~Shared_SemMutex();
 };
 
+/**
+ * Semlock RAII container
+ *   lock_guard<LOCK_TYPE>
+ *   shared_lock<LOCK_TYPE>
+ * */
+
+template <typename LOCK_TYPE>
+class lock_guard {
+  typedef typename std::enable_if<std::is_base_of<SemMutex, LOCK_TYPE>::value,
+                                  LOCK_TYPE>::type LOCK;
+
+ private:
+  LOCK &lock_;
+
+ public:
+  lock_guard() = delete;
+  lock_guard(LOCK &lock) : lock_(lock) { lock_.lock(); }
+  ~lock_guard() { lock_.unlock(); }
+};
+
 template <typename LOCK_TYPE>
 class shared_lock {
+  typedef typename std::enable_if<
+      std::is_base_of<Shared_SemMutex, LOCK_TYPE>::value, LOCK_TYPE>::type
+      SHARED_LOCK;
+
  private:
-  LOCK_TYPE &lock_;
+  SHARED_LOCK &lock_;
 
  public:
   shared_lock() = delete;
-  shared_lock(LOCK_TYPE &lock)
-      : lock_(lock) {
-    lock_.shared_lock();
-  }
+  shared_lock(SHARED_LOCK &lock) : lock_(lock) { lock_.shared_lock(); }
   ~shared_lock() { lock_.shared_unlock(); }
 };
 
