d6ad540ab12d27787b0bee050bf4411d11883f46
dangoll_workstation <dango_liang@qq.com>
2020-08-03 18:30:11 +0800

    @ChangeList 1. 融合debug draw到工程

---
+++ modules/neo_adp/display_elements/display_implements.hpp
@@ -0,0 +1,144 @@
+#pragma once
+
+#include "common_frame.hpp"
+
+static void drawLabel(const ThreadData &td, ddVec3_In pos, const char *name) {
+  if (!keys.showLabels) {
+    return;
+  }
+
+  // Only draw labels inside the camera frustum.
+  if (camera.isPointInsideFrustum(pos[0], pos[1], pos[2])) {
+    const ddVec3 textColor = {0.8f, 0.8f, 1.0f};
+    dd::projectedText(td.ddContext, name, pos, textColor,
+                      toFloatPtr(camera.vpMatrix), 0, 0, WindowWidth,
+                      WindowHeight, 0.5f);
+  }
+}
+
+static void drawGrid(const ThreadData &td) {
+  if (keys.showGrid) {
+    dd::xzSquareGrid(td.ddContext, -50.0f, 50.0f, 0.0f, 1.0f,
+                     dd::colors::DarkGreen);  // Grid from -50 to +50 in both X & Z
+  }
+}
+
+static void drawMiscObjects(const ThreadData &td) {
+  // Start a row of objects at this position:
+  ddVec3 origin = {-15.0f, 0.0f, 0.0f};
+
+  // Box with a point at it's center:
+  drawLabel(td, origin, "box");
+  dd::box(td.ddContext, origin, dd::colors::Blue, 1.5f, 1.5f, 1.5f);
+  dd::point(td.ddContext, origin, dd::colors::White, 15.0f);
+  origin[0] += 3.0f;
+
+  // Sphere with a point at its center
+  drawLabel(td, origin, "sphere");
+  dd::sphere(td.ddContext, origin, dd::colors::Red, 1.0f);
+  dd::point(td.ddContext, origin, dd::colors::White, 15.0f);
+  origin[0] += 4.0f;
+
+  // Two cones, one open and one closed:
+  const ddVec3 condeDir = {0.0f, 2.5f, 0.0f};
+  origin[1] -= 1.0f;
+
+  drawLabel(td, origin, "cone (open)");
+  dd::cone(td.ddContext, origin, condeDir, dd::colors::Yellow, 1.0f, 2.0f);
+  dd::point(td.ddContext, origin, dd::colors::White, 15.0f);
+  origin[0] += 4.0f;
+
+  drawLabel(td, origin, "cone (closed)");
+  dd::cone(td.ddContext, origin, condeDir, dd::colors::Cyan, 0.0f, 1.0f);
+  dd::point(td.ddContext, origin, dd::colors::White, 15.0f);
+  origin[0] += 4.0f;
+
+  // Axis-aligned bounding box:
+  const ddVec3 bbMins = {-1.0f, -0.9f, -1.0f};
+  const ddVec3 bbMaxs = {1.0f, 2.2f, 1.0f};
+  const ddVec3 bbCenter = {(bbMins[0] + bbMaxs[0]) * 0.5f,
+                           (bbMins[1] + bbMaxs[1]) * 0.5f,
+                           (bbMins[2] + bbMaxs[2]) * 0.5f};
+  drawLabel(td, origin, "AABB");
+  dd::aabb(td.ddContext, bbMins, bbMaxs, dd::colors::Orange);
+  dd::point(td.ddContext, bbCenter, dd::colors::White, 15.0f);
+
+  // Move along the Z for another row:
+  origin[0] = -15.0f;
+  origin[2] += 5.0f;
+
+  // A big arrow pointing up:
+  const ddVec3 arrowFrom = {origin[0], origin[1], origin[2]};
+  const ddVec3 arrowTo = {origin[0], origin[1] + 5.0f, origin[2]};
+  drawLabel(td, arrowFrom, "arrow");
+  dd::arrow(td.ddContext, arrowFrom, arrowTo, dd::colors::Magenta, 1.0f);
+  dd::point(td.ddContext, arrowFrom, dd::colors::White, 15.0f);
+  dd::point(td.ddContext, arrowTo, dd::colors::White, 15.0f);
+  origin[0] += 4.0f;
+
+  // Plane with normal vector:
+  const ddVec3 planeNormal = {0.0f, 1.0f, 0.0f};
+  drawLabel(td, origin, "plane");
+  dd::plane(td.ddContext, origin, planeNormal, dd::colors::Yellow,
+            dd::colors::Blue, 1.5f, 1.0f);
+  dd::point(td.ddContext, origin, dd::colors::White, 15.0f);
+  origin[0] += 4.0f;
+
+  // Circle on the Y plane:
+  drawLabel(td, origin, "circle");
+  dd::circle(td.ddContext, origin, planeNormal, dd::colors::Orange, 1.5f,
+             15.0f);
+  dd::point(td.ddContext, origin, dd::colors::White, 15.0f);
+  origin[0] += 3.2f;
+
+  // Tangent basis vectors:
+  const ddVec3 normal = {0.0f, 1.0f, 0.0f};
+  const ddVec3 tangent = {1.0f, 0.0f, 0.0f};
+  const ddVec3 bitangent = {0.0f, 0.0f, 1.0f};
+  origin[1] += 0.1f;
+  drawLabel(td, origin, "tangent basis");
+  dd::tangentBasis(td.ddContext, origin, normal, tangent, bitangent, 2.5f);
+  dd::point(td.ddContext, origin, dd::colors::White, 15.0f);
+
+  // And a set of intersecting axes:
+  origin[0] += 4.0f;
+  origin[1] += 1.0f;
+  drawLabel(td, origin, "cross");
+  dd::cross(td.ddContext, origin, 2.0f);
+  dd::point(td.ddContext, origin, dd::colors::White, 15.0f);
+}
+
+static void drawFrustum(const ThreadData &td) {
+  const ddVec3 color = {0.8f, 0.3f, 1.0f};
+  const ddVec3 origin = {-8.0f, 0.5f, 14.0f};
+  drawLabel(td, origin, "frustum + axes");
+
+  // The frustum will depict a fake camera:
+  const Matrix4 proj =
+      Matrix4::perspective(degToRad(45.0f), 800.0f / 600.0f, 0.5f, 4.0f);
+  const Matrix4 view =
+      Matrix4::lookAt(Point3(-8.0f, 0.5f, 14.0f), Point3(-8.0f, 0.5f, -14.0f),
+                      Vector3::yAxis());
+  const Matrix4 clip = inverse(proj * view);
+  dd::frustum(td.ddContext, toFloatPtr(clip), color);
+
+  // A white dot at the eye position:
+  dd::point(td.ddContext, origin, dd::colors::White, 15.0f);
+
+  // A set of arrows at the camera's origin/eye:
+  const Matrix4 transform = Matrix4::translation(Vector3(-8.0f, 0.5f, 14.0f)) *
+                            Matrix4::rotationZ(degToRad(60.0f));
+  dd::axisTriad(td.ddContext, toFloatPtr(transform), 0.3f, 2.0f);
+}
+
+static void drawText(const ThreadData &td) {
+  // HUD text:
+  const ddVec3 textColor = {1.0f, 1.0f, 1.0f};
+  const ddVec3 textPos2D = {10.0f, 15.0f, 0.0f};
+  dd::screenText(
+      td.ddContext,
+      "Welcome to the multi-threaded Core OpenGL Debug Draw demo.\n\n"
+      "[SPACE]  to toggle labels on/off\n"
+      "[RETURN] to toggle grid on/off",
+      textPos2D, textColor, 0.55f);
+}
--- modules/neo_adp/main.cc
+++ modules/neo_adp/main.cc
@@ -1,85 +1,19 @@
-#include "modules/neo_adp/display_elements/common_frame.h"
+#include "modules/neo_adp/display_elements/display_implements.hpp"
 
 /* main function */
 int main(int args, char** argv) {
-  // Our state
-  bool ImGui = true;
-  bool the_same_color = false;
-  bool draw_trangle_without_render = false;
-  bool draw_trangle = false;
-  bool bonus_draw_line = false;
-  bool bonus_draw_another_trangle = false;
-  unsigned int VBO, VAO, EBO;
-  bool show_demo_window = true;
+  atd::utility::Singleton::try_register<OpenGL_Frame>();
+  
+  atd::utility::Singleton::instance<OpenGL_Frame>()->register_CallBack(
+      &drawGrid);
+  atd::utility::Singleton::instance<OpenGL_Frame>()->register_CallBack(
+      &drawMiscObjects);
+  atd::utility::Singleton::instance<OpenGL_Frame>()->register_CallBack(
+      &drawFrustum);
+  atd::utility::Singleton::instance<OpenGL_Frame>()->register_CallBack(
+      &drawText);
 
-  GLuint VertexArrayID;
-  glGenVertexArrays(1, &VertexArrayID);
-  glBindVertexArray(VertexArrayID);
+  atd::utility::Singleton::instance<OpenGL_Frame>()->spin();
 
-  GLuint programID = LoadShaders("SimpleVertexShader.vertexshader",
-                                 "SimpleFragmentShader.fragmentshader");
-  ImVec4 v1 = ImVec4(-0.25f, -0.25f, 0.0f, 1.00f);
-  ImVec4 v2 = ImVec4(0.25f, -0.25f, 0.0f, 1.00f);
-  ImVec4 v3 = ImVec4(0.0f, 0.25f, 0.0f, 1.00f);
-
-  GLuint vertexbuffer;
-
-  // Main loop
-  while (!glfwWindowShouldClose(ptr_window)) {
-
-    /* 创建 VBO */
-    GLfloat g_vertex_buffer_data[] = {
-        v1.x, v1.y, v1.z, v2.x, v2.y, v2.z, v3.x, v3.y, v3.z,
-    };
-    glGenBuffers(1, &vertexbuffer);
-    glBindBuffer(GL_ARRAY_BUFFER,
-                 vertexbuffer); /* 顶点缓冲对象 GL_ARRAY_BUFFER */
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data),
-                 g_vertex_buffer_data, GL_STATIC_DRAW);
-    /**
-     * GL_STATIC_DRAW ：数据不会或几乎不会改变。
-     * GL_DYNAMIC_DRAW：数据会被改变很多。
-     * GL_STREAM_DRAW ：数据每次绘制时都会改变。*/
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
-                          (void*)0);
-    /**
-     * 第一个参数指定我们要配置的顶点属性。顶点着色器中使用layout(location=0)定义了position顶点属性的位置值(Location)
-     * 第二个参数指定顶点属性的大小。顶点属性是一个vec3，它由3个值组成，所以大小是
-     * 第三个参数指定数据的类型，这里是GL_FLOAT(GLSL中vec*都是由浮点数值组成的)。
-     * 第四个参数定义我们是否希望数据被标准化(Normalize)。如果我们设置为GL_TRUE，所有数据都会被映射到0（对于有符号型signed数据是-1）到1之间。我们把它设置为GL_FALSE
-     * 第五个参数叫做步长(Stride)，它告诉我们在连续的顶点属性组之间的间隔。由于下个组位置数据在3个float之后，我们把步长设置为3sizeof(float)。要注意的是由于我们知道这个数组是紧密排列的（在两个顶点属性之间没有空隙）我们也可以设置为0来让OpenGL决定具体步长是多少（只有当数值是紧密排列时才可用）。一旦我们有更多的顶点属性，我们就必须更小心地定义每个顶点属性之间的间隔
-     * 最后一个参数的类型是void*，所以需要我们进行这个奇怪的强制类型转换。它表示位置数据在缓冲中起始位置的偏移量(Offset)。由于位置数据在数组的开头，所以这里是0。
-     */
-    glEnableVertexAttribArray(0);
-    glUseProgram(programID); /* 每个着色器调用和渲染调用都会使用这个着色器了*/
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplGlfw_NewFrame();
-    ImGui::NewFrame();
-    ImGui::Begin("change vertex", &ImGui, ImGuiWindowFlags_MenuBar);
-    ImGui::SliderFloat("", &v3.y, -1.0f, 1.0f, "v3.y = %.3f");
-    ImGui::End();
-
-    if (show_demo_window) ImGui::ShowDemoWindow(&show_demo_window);
-
-    // Rendering
-    ImGui::Render();
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-  }
-
-  // Cleanup
-  glDeleteVertexArrays(1, &VAO);
-  glDeleteBuffers(1, &VBO);
-  glDeleteBuffers(1, &EBO);
-
-  ImGui_ImplOpenGL3_Shutdown();
-  ImGui_ImplGlfw_Shutdown();
-  ImGui::DestroyContext();
-
-  glfwDestroyWindow(ptr_window);
-  glfwTerminate();
-
-  return 0;
+  exit(EXIT_SUCCESS);
 }
